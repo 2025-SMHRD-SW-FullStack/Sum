ce9f1f50ac931a515532fc57ab45a87751f99bdf
Sojeong <Sojeongg0426@gmail.com>
2025-05-01 08:53:20 +0900

    0501_1

--- Cash/src/Cash/CashDAO.java
+++ Cash/src/Cash/CashDAO.java
@@ -1,9 +1,77 @@
 package Cash;
 
-public class CashDAO {
-	public static void main(String[] args) {
-		System.out.println("커밋 확인용");    
-		
-		
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CashDAO extends DBDAO{
+
+	
+
+	// 회원가입 메소드
+	public int join(String user_id, String user_pw, String user_name) {
+		int result = 0;
+
+		try {
+
+			getConn();
+
+			String sql = "INSERT INTO USER_INFO	 VALUES(?,?,?)";
+
+			psmt = conn.prepareStatement(sql);
+
+			// ? 인자의 시작인덱스는 1이다
+			psmt.setString(1, user_id); // user_id 입력
+			psmt.setString(2, user_pw); // 입력받은
+			psmt.setString(3, user_name); // 입력받은 name
+
+			result = psmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+
+			getClose();
+
+		}
+
+		return result;
+
 	}
+
+	// 로그인 메소드
+	public CashDTO login(String user_id, String user_pw) {
+
+		CashDTO dto = null;
+		try {
+			getConn();
+
+			String sql = "SELECT * FROM USER_INFO WHERE USER_ID = ? AND USER_PW= ?";
+			psmt = conn.prepareStatement(sql);
+
+			psmt.setString(1, user_id);
+			psmt.setString(2, user_pw);
+			rs = psmt.executeQuery();
+			if (rs.next()) {
+				dto = new CashDTO();
+				dto.setUser_id(rs.getString("user_id"));
+				dto.setUser_name(rs.getString("user_name"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+
+			getClose();
+
+		}
+
+		return dto;
+
+	}
+
+	
+
 }
--- Cash/src/Cash/PlayMethod.java
+++ Cash/src/Cash/PlayMethod.java
@@ -1,5 +1,63 @@
 package Cash;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class PlayMethod {
+	Random rd = new Random();
+
+	private int fprice = 0; // 최종 금액
+	private int refund = 0; // 환불금
+
+	public ArrayList<ObjectVO> object(int level) {
+		// 개수 변경을 위해 ArrayList에 적음
+		ArrayList<ObjectVO> objectList = new ArrayList<ObjectVO>();
+
+		int radishNum = rd.nextInt(10) + 1; // 변경되는 무 값 5-> 무 개당 가격: 500,550,525
+		int radishCount; // 무 개수
+		int radishPrice = radishNum * 100; // 무 개당 가격
+
+		int objectCount = 5;
+		if (level == 2) { // Normal
+			objectCount = 10;
+			radishPrice = radishNum * 110;
+		} else if (level == 3) { // Hard
+			objectCount = 20;
+			radishPrice = radishNum * 115;
+		}
+
+		radishCount = rd.nextInt(objectCount) + 1;
+
+		// list에서 가격이랑 개수를 빼와서 main에서 곱해야함!!!!!!!!!!!!!!!
+		objectList.add(new ObjectVO("가구", 10000, rd.nextInt(objectCount) + 1));
+		objectList.add(new ObjectVO("생선", 5000, rd.nextInt(objectCount) + 1));
+		objectList.add(new ObjectVO("곤충", 7000, rd.nextInt(objectCount) + 1));
+		objectList.add(new ObjectVO("과일", 6000, rd.nextInt(objectCount) + 1));
+		objectList.add(new ObjectVO("꽃", 3000, rd.nextInt(objectCount) + 1));
+		objectList.add(new ObjectVO("무", radishPrice, radishCount));
+
+		return objectList;
+	}
+
+	public int refundRandom(int level) {
+		// 물품 환불 확률 (부분환불/전체환불/환불x)
+		ArrayList<ObjectVO> objectList = new ArrayList<ObjectVO>();
+
+		objectList = object(level);
+
+		int refundNum = rd.nextInt(100) + 1; // 환불 받을 확률
+		int index = rd.nextInt(objectList.size());
+
+		if (refundNum <= 20) { // 부분 환불
+			refund /= 2;
+			if (refundNum <= 5) {
+				refund = objectList.get(index).getObjectSel();
+			}
+		} else
+			refund = 0;
+
+		return refund;
+		// 최종값에서 환불금 뺀 금액이 시재에 저장되어야하기에 환불금을 리턴 해줌
+	}
 
 }
